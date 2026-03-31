54d1dd83f39f23ce27f48297655fdfd693bb8112
This zrex <lordnazri@protonmail.com>
2026-03-31 22:22:18 +0700

    feat: implement materi pengenalan array

---
+++ materi-7-pengenalan-array/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] n = new int[10];
+int i, j;
+for (i = 0; i < 10; i++)
+{
+    n[i] = i + 100;
+}
+for (j = 0; j < 10; j++)
+{
+    Console.WriteLine("Element[{0}] = {1}", j, n[j]);
+
+}
+Console.ReadKey();
